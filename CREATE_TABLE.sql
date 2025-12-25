0356f5e52f0d8dd0de499dd8c67676ac2087ba68
aisen <aisenew321@gmail.com>
2025-12-25 17:35:27 +0900

    tablica

---
+++ CREATE_TABLE.sql
@@ -0,0 +1,30 @@
+CREATE TABLE institutes
+(
+id SERIAL PRIMARY KEY,
+name VARCHAR(1024) NOT NULL
+);
+
+
+
+
+CREATE TABLE groups_  
+(
+id SERIAL PRIMARY KEY,
+name VARCHAR(1024) NOT NULL,
+short_name VARCHAR (64) NOT NULL UNIQUE,
+year INT NOT NULL,
+institute_id INT NOT NULL
+);
+
+
+
+
+
+CREATE TABLE students   
+(
+id SERIAL PRIMARY KEY,
+surname VARCHAR(512) NOT NULL,
+name VARCHAR(1024) NOT NULL,
+father_name VARCHAR (512),
+group_id INT NOT NULL
+);
